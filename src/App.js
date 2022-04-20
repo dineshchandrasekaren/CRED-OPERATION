dc96f5b132090166a3f2c6f321397a06f7926bee
dineshchandrasekaren <dineshchandrasekaren@gmail.com>
2022-04-20 16:13:15 +0530

    like added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { data } from "./data";
+import {
+  AiOutlineHeart as Outline,
+  AiFillHeart as Filled,
+} from "react-icons/ai";
+class App extends Component {
+  state = {
+    movies: [...data],
+  };
+  onDelete = (id) => {
+    let movies = this.state.movies;
+    movies = movies.filter((mov) => mov._id !== id);
+    this.setState({ movies });
+  };
+  handleLike = (id) => {
+    let movies = this.state.movies;
+    movies[id].isLiked = !movies[id].isLiked;
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    this.setState({ movies });
+  };
+
+  render() {
+    return (
+      <main className="container">
+        <p className="fs-4">{`Showing ${
+          this.state.movies.length ? this.state.movies.length : "no"
+        } movies in the Database`}</p>
+        {this.movies()}
+      </main>
+    );
+  }
+
+  movies() {
+    return (
+      <table class="table table-hover">
+        <thead>
+          <tr>
+            <th scope="col">Title</th>
+            <th scope="col">Genre</th>
+            <th scope="col">Stock</th>
+            <th scope="col">Rate</th>
+          </tr>
+        </thead>
+        <tbody>
+          {this.state.movies.map((movie, i) => {
+            return (
+              <tr key={movie._id}>
+                <td>{movie.title}</td>
+                <td>{movie.genre.name}</td>
+                <td>{movie.numberInStock}</td>
+                <td>{movie.dailyRentalRate}</td>
+                <td onClick={() => this.handleLike(i)}>
+                  {movie.isLiked ? <Filled size={20} /> : <Outline size={20} />}
+                </td>
+                <td onClick={() => this.onDelete(movie._id)}>
+                  <button className="btn  btn-danger">Delete</button>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    );
+  }
 }
 
 export default App;
